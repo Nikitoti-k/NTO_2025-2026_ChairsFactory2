1a95d162433e450e83826a4aef988cfde64d1fe4
1lushka <il.zah@mail.ru>
2026-02-15 23:03:03 +0300

    Добавил скрипты для компьютера

---
+++ Assets/Scripts/Computer/MineralTypeData.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MineralTypeData
+{
+    public string title;
+    [TextArea] public List<string> description;
+}
