dead41126d3ad8ada125fa803cd2be27d97de3a9
GGrriisshha <grishasidorov2010@gmail.com>
2026-04-08 11:21:35 +0300

    Кассеты

--- Assets/Scripts/Scaner.cs
+++ Assets/Scripts/Scaner.cs
@@ -4,43 +4,42 @@ public class CleanSpawner : MonoBehaviour
 {
     public GameObject unit;
     public Vector3 spawnPosition = new Vector3(0, 5, 0);
-    public float lifetime = 5f; // Максимальное время жизни клона
+    public float lifetime = 5f; 
 
-    private GameObject currentClone; // Храним ссылку на созданный клон
+    private GameObject currentClone;
 
     private void OnTriggerEnter(Collider other)
     {
-        // Игнорируем сами клоны, чтобы не было бесконечного цикла
+
         if (other.gameObject.name.EndsWith("(Pure)")) return;
 
-        // Запоминаем объект, который вошел
+
         unit = other.gameObject;
 
-        // 1. Создаем копию
+
         currentClone = Instantiate(unit, spawnPosition, unit.transform.rotation);
         currentClone.name = unit.name + " (Pure)";
         currentClone.transform.SetParent(null);
 
-        // 2. Очищаем от логики (оставляем только визуал)
+
         CleanLogic(currentClone);
 
-        // 3. Уничтожаем через заданное время (если объект не выйдет раньше)
+
         Destroy(currentClone, lifetime);
 
-        Debug.Log($"Создана копия {currentClone.name}. Исчезнет через {lifetime} сек или при выходе из зоны.");
+        Debug.Log($"={currentClone.name}. Исчезнет через {lifetime}");
     }
 
-    // Срабатывает в момент "отрыва" / выхода из зоны
     private void OnTriggerExit(Collider other)
     {
-        // Если зону покинул именно тот объект, который мы копировали
+
         if (unit != null && other.gameObject == unit)
         {
             if (currentClone != null)
             {
-                // Время на ноль: удаляем клон немедленно
+
                 Destroy(currentClone);
-                Debug.Log("Связь разорвана: клон удален.");
+                Debug.Log("клон удален.");
             }
         }
         
---
+++ Assets/casette.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Cassette : MonoBehaviour
+{
+    public enum CassetteType { Type1, Type2, Type3, Type4 }
+    public CassetteType type;
+}
