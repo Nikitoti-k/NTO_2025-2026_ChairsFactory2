5848ffe2d9da30f8c5b4896148674f441fa614d7
1lushka <il.zah@mail.ru>
2026-04-08 15:03:27 +0300

    Test

--- Assets/Scripts/SaveSystem/SaveManager.cs
+++ Assets/Scripts/SaveSystem/SaveManager.cs
@@ -383,7 +383,7 @@ public class SaveManager : MonoBehaviour
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        if (!scene.name.Equals("GameScene"))
+        if (!scene.name.Equals("GameScene") && !scene.name.Equals("TestScene"))
             return;
 
         if (pendingLoadSlot != -1)
